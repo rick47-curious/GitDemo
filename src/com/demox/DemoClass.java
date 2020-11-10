8cc9c408dca73a79bde5be5cda967b47cd6a16d3
Anuplab Chatterjee <anuplab.ck@gmail.com>
2020-11-10 16:06:45 +0530

    Indian guy commits

---
+++ src/com/demox/DemoClass.java
@@ -0,0 +1,12 @@
+package com.demox;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		System.out.println("goodbye, world!");
+
+	}
+
+}
