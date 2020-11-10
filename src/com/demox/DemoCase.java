95ccff8513a993965181ad6c2d83ec509f61b272
Anuplab Chatterjee <anuplab.ck@gmail.com>
2020-11-10 17:59:02 +0530

    Demo IST Architect

---
+++ src/com/demox/DemoCase.java
@@ -0,0 +1,13 @@
+package com.demox;
+
+public class DemoCase {
+
+	public static void main(String[] args) {
+		
+		System.out.println("New case begins...");
+		
+		System.out.println("New commit waits....");
+
+	}
+
+}
---
+++ src/com/demox/DemoTest.java
@@ -0,0 +1,11 @@
+package com.demox;
+
+public class DemoTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Demo Tests here....");
+
+	}
+
+}
